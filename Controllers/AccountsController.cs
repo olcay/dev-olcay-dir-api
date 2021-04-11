9ea27e6a3800c49fec3b72e0034cc5b9aaa8c1dd
olcay <olcaybayram@gmail.com>
2021-04-11 15:52:54 +0200

    Set SameSiteMode to None
    Update cookie options to work from another domain

--- Controllers/AccountsController.cs
+++ Controllers/AccountsController.cs
@@ -157,7 +157,9 @@ namespace WebApi.Controllers
             var cookieOptions = new CookieOptions
             {
                 HttpOnly = true,
-                Expires = DateTime.UtcNow.AddDays(7)
+                Expires = DateTime.UtcNow.AddDays(7),
+                SameSite = SameSiteMode.None,
+                Secure = true
             };
             Response.Cookies.Append("refreshToken", token, cookieOptions);
         }
